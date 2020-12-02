d9c058830c57a21d0b1ec4375f23c0631265bb5b
Nils-Emil Lille <nilsemil.lille@gmail.com>
2020-12-02 17:17:30 +0200

    Day 1 & Day 2

---
+++ src/test/java/day2/PasswordValidatorTest.java
@@ -0,0 +1,29 @@
+package day2;
+
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class PasswordValidatorTest {
+
+
+    @Test
+    public void hasExactlyOneCharacterAtSpecifiedPositions_case1() {
+        boolean b = PasswordValidator.hasExactlyOneCharacterAtSpecifiedPositions("1-3 a: abcde");
+        assertTrue(b);
+    }
+
+    @Test
+    public void hasExactlyOneCharacterAtSpecifiedPositions_case2() {
+        boolean b = PasswordValidator.hasExactlyOneCharacterAtSpecifiedPositions("1-3 b: cdefg");
+        assertFalse(b);
+    }
+
+    @Test
+    public void hasExactlyOneCharacterAtSpecifiedPositions_case3() {
+        boolean b = PasswordValidator.hasExactlyOneCharacterAtSpecifiedPositions("2-9 c: ccccccccc");
+        assertFalse(b);
+    }
+}
