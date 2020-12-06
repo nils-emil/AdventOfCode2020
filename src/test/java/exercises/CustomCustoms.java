b07b9bbe78f7d3e8d0ad7b9accc0f2abee358dfe
Nils-Emil Lille <nilsemil.lille@gmail.com>
2020-12-06 07:52:39 +0200

    Day5

---
+++ src/test/java/exercises/CustomCustoms.java
@@ -0,0 +1,16 @@
+package exercises;
+
+import org.junit.Test;
+
+import java.io.FileNotFoundException;
+
+import static org.junit.Assert.assertEquals;
+
+public class CustomCustoms {
+
+    @Test
+    public void findMySeatId() throws FileNotFoundException {
+        assertEquals(2947, CustomsCustoms.getSumOfCollectiveAnswers("day6.csv"));
+    }
+
+}
