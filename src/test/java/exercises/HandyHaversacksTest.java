0a4d97246832a6c0e3da4890b5ff5a53c5412d1d
Nils-Emil Lille <nilsemil.lille@gmail.com>
2020-12-07 11:14:34 +0200

    Day7 solution

---
+++ src/test/java/exercises/HandyHaversacksTest.java
@@ -0,0 +1,36 @@
+package exercises;
+
+import org.junit.Test;
+
+import java.io.FileNotFoundException;
+
+import static org.junit.Assert.assertEquals;
+
+public class HandyHaversacksTest {
+
+    @Test
+    public void howManyColorsCanHoldGoldenBag_case1() throws FileNotFoundException {
+        assertEquals(229, HandyHaversacks.howManyColorsCanHoldGoldenBag("day7.csv"));
+    }
+
+    @Test
+    public void howManyColorsCanHoldGoldenBag_case2() throws FileNotFoundException {
+        assertEquals(4, HandyHaversacks.howManyColorsCanHoldGoldenBag("day7_test.csv"));
+    }
+
+    @Test
+    public void howManyIndividualBagsInsideGoldenBag_case1() throws FileNotFoundException {
+        assertEquals(6683, HandyHaversacks.howManyIndividualBagsInsideGoldenBag("day7.csv"));
+    }
+
+    @Test
+    public void howManyIndividualBagsInsideGoldenBag_case2() throws FileNotFoundException {
+        assertEquals(32, HandyHaversacks.howManyIndividualBagsInsideGoldenBag("day7_test.csv"));
+    }
+
+    @Test
+    public void howManyIndividualBagsInsideGoldenBag_case2_case3() throws FileNotFoundException {
+        assertEquals(126, HandyHaversacks.howManyIndividualBagsInsideGoldenBag("day7_test2.csv"));
+    }
+
+}
