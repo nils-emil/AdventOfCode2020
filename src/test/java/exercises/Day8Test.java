41ed4e724abfa20def749e3a8e24888ab05eaabc
Nils-Emil Lille <nilsemil.lille@gmail.com>
2020-12-08 09:05:08 +0200

    Day8

---
+++ src/test/java/exercises/Day8Test.java
@@ -0,0 +1,20 @@
+package exercises;
+
+import org.junit.Test;
+
+import java.io.FileNotFoundException;
+
+import static org.junit.Assert.assertEquals;
+
+public class Day8Test {
+
+    @Test
+    public void findMySeatId() throws FileNotFoundException {
+        assertEquals(969, Day8.getAccumulationUntilLastLine("day8.csv"));
+    }
+    @Test
+    public void findMySeatId_test() throws FileNotFoundException {
+        assertEquals(661, Day8.getAccumulationUntilLastLine("day8_test.csv"));
+    }
+
+}
