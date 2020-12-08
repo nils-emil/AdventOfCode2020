a4bef24d93183e5279ce998ed9a4fd042c814b67
Nils-Emil Lille <nilsemil.lille@gmail.com>
2020-12-08 09:14:54 +0200

    Day8 test names

--- src/test/java/exercises/Day8Test.java
+++ src/test/java/exercises/Day8Test.java
@@ -9,12 +9,12 @@ import static org.junit.Assert.assertEquals;
 public class Day8Test {
 
     @Test
-    public void findMySeatId() throws FileNotFoundException {
+    public void getAccumulationUntilLastLine_case1() throws FileNotFoundException {
         assertEquals(969, Day8.getAccumulationUntilLastLine("day8.csv"));
     }
 
     @Test
-    public void findMySeatId_test() throws FileNotFoundException {
+    public void getAccumulationUntilLastLine_case2() throws FileNotFoundException {
         assertEquals(8, Day8.getAccumulationUntilLastLine("day8_test.csv"));
     }
 
