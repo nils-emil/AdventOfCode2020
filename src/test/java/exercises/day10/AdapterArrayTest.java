ee7868fe14d90c75b793088a6caf382c669ac93d
Nils-Emil Lille <nilsemil.lille@gmail.com>
2020-12-10 09:38:45 +0200

    Day 10

---
+++ src/test/java/exercises/day10/AdapterArrayTest.java
@@ -0,0 +1,30 @@
+package exercises.day10;
+
+import org.junit.Test;
+
+import java.io.FileNotFoundException;
+
+import static org.junit.Assert.assertEquals;
+
+public class AdapterArrayTest {
+
+    @Test
+    public void getJoltMultiplication_case1() throws FileNotFoundException {
+        assertEquals(2048, AdapterArray.getJoltDifferenceMultiplication("day10.csv"));
+    }
+
+    @Test
+    public void getJoltMultiplication_case2() throws FileNotFoundException {
+        assertEquals(35, AdapterArray.getJoltDifferenceMultiplication("day10_test.csv"));
+    }
+
+    @Test
+    public void countNumberOfWays_case1() throws FileNotFoundException {
+        assertEquals(1322306994176L, AdapterArray.countNumberOfWays("day10.csv"));
+    }
+
+    @Test
+    public void countNumberOfWays_case2() throws FileNotFoundException {
+        assertEquals(8L, AdapterArray.countNumberOfWays("day10_test.csv"));
+    }
+}
