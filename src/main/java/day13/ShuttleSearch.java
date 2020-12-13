26cde2889fe589dda899028435a82cfd0e773269
Nils-Emil Lille <nilsemil.lille@gmail.com>
2020-12-13 11:04:07 +0200

    Day 13 part two

--- src/main/java/day13/ShuttleSearch.java
+++ src/main/java/day13/ShuttleSearch.java
@@ -5,10 +5,12 @@ import utils.FileUtils;
 
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class ShuttleSearch {
-    public static int getManhattanDistance(String inputfile) throws FileNotFoundException {
+
+    public static int getEarliestBusToTake(String inputfile) throws FileNotFoundException {
         List<String> lines = FileUtils.getCsvLines(inputfile);
         int timestamp = Integer.parseInt(lines.get(0));
         String[] busses = lines.get(1).split(",");
@@ -29,5 +31,43 @@ public class ShuttleSearch {
             }
         }
         return currentBusId * (currentEarliest - timestamp);
+    }
+
+
+    public static long getEarliestSbsequentTimeStamp(String inputfile) throws FileNotFoundException {
+        List<String> lines = FileUtils.getCsvLines(inputfile);
+        String[] busses = lines.get(1).split(",");
+        List<Long> busIds = new ArrayList<>();
+        HashMap<Long, Long> busOffsets = new HashMap<>();
+        long largestBusId = 0;
+        long largestBusIdIndex = 0;
+        long index = 0;
+        for (String bus : busses) {
+            if (bus.chars().allMatch(Character::isDigit)) {
+                long e = Long.parseLong(bus);
+                busIds.add(e);
+                busOffsets.put(e, index);
+                if (e > largestBusId) {
+                    largestBusId = e;
+                    largestBusIdIndex = index;
+                }
+            }
+            index ++;
+        }
+        long nextTimestampToCheck = 0L;
+        while (true) {
+            nextTimestampToCheck += largestBusId;
+            long numberOfBussesThatMatched = 0;
+            for (long busId : busIds) {
+                long busOffset = busOffsets.get(busId);
+                long timeStampBusIdShouldMatch = nextTimestampToCheck - largestBusIdIndex + busOffset;
+                if (timeStampBusIdShouldMatch % busId == 0) {
+                    numberOfBussesThatMatched++;
+                }
+            }
+            if (numberOfBussesThatMatched == busIds.size()) {
+                return nextTimestampToCheck - largestBusIdIndex;
+            }
+        }
     }
 }
--- src/test/java/day13/ShuttleSearchTest.java
+++ src/test/java/day13/ShuttleSearchTest.java
@@ -1,23 +1,55 @@
 package day13;
 
-import day12.RainRisk;
 import org.junit.Assert;
 import org.junit.Test;
 
 import java.io.FileNotFoundException;
 
-import static org.junit.Assert.assertEquals;
-
 public class ShuttleSearchTest {
 
     @Test
     public void part1_case1() throws FileNotFoundException {
-        Assert.assertEquals(25, ShuttleSearch.getManhattanDistance("day13_test.csv"));
+        Assert.assertEquals(295, ShuttleSearch.getEarliestBusToTake("day13_test.csv"));
     }
 
     @Test
     public void part1_case2() throws FileNotFoundException {
         // not 1007158
-        assertEquals(2165, ShuttleSearch.getManhattanDistance("day13.csv"));
+        Assert.assertEquals(2165, ShuttleSearch.getEarliestBusToTake("day13.csv"));
+    }
+
+    @Test
+    public void part2_case1() throws FileNotFoundException {
+        Assert.assertEquals(1068781, ShuttleSearch.getEarliestSbsequentTimeStamp("day13_test.csv"));
+    }
+
+    @Test
+    public void part2_case2() throws FileNotFoundException {
+        Assert.assertEquals(3417, ShuttleSearch.getEarliestSbsequentTimeStamp("day13_test2.csv"));
+    }
+
+    @Test
+    public void part2_case3() throws FileNotFoundException {
+        Assert.assertEquals(754018, ShuttleSearch.getEarliestSbsequentTimeStamp("day13_test3.csv"));
+    }
+
+    @Test
+    public void part2_case4() throws FileNotFoundException {
+        Assert.assertEquals(779210, ShuttleSearch.getEarliestSbsequentTimeStamp("day13_test4.csv"));
+    }
+
+    @Test
+    public void part2_case5() throws FileNotFoundException {
+        Assert.assertEquals(1261476, ShuttleSearch.getEarliestSbsequentTimeStamp("day13_test5.csv"));
+    }
+
+    @Test
+    public void part2_case6() throws FileNotFoundException {
+        Assert.assertEquals(1202161486, ShuttleSearch.getEarliestSbsequentTimeStamp("day13_test6.csv"));
+    }
+
+    @Test
+    public void part2_case7() throws FileNotFoundException {
+        Assert.assertEquals(1202161486, ShuttleSearch.getEarliestSbsequentTimeStamp("day13.csv"));
     }
 }
