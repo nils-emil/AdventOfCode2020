d79ba9595132d7d669f023e03b9f54b16b54cae2
Nils-Emil Lille <nilsemil.lille@gmail.com>
2020-12-24 16:58:43 +0200

    Day 16 part 2

--- src/main/java/day16/TicketTranslationPart2.java
+++ src/main/java/day16/TicketTranslationPart2.java
@@ -6,27 +6,93 @@ import utils.FileUtils;
 
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
+import java.util.stream.Collectors;
 
-public class TicketTranslation {
+public class TicketTranslationPart2 {
 
     public static long getMemorySum(String inputfile) throws FileNotFoundException {
         List<String> lines = FileUtils.getCsvLines(inputfile);
         HashMap<String, List<Pair<Integer, Integer>>> ranges = getFieldRanges(lines);
+        List<String> fields = getFields(lines);
+        String myTicket = getMyTicket(lines);
+        String[] split = myTicket.split(",");
         List<String> nearbyTicket = getNearbyTickets(lines);
-        List<List<Integer>> nearbyTickets = parseTickets(nearbyTicket);
         List<Pair<Integer, Integer>> allranges = getAllRanges(ranges);
-        int sum = 0;
-        for (List<Integer> tcket : nearbyTickets) {
-            sum += isValueValue(tcket, allranges)
-                    .stream()
-                    .reduce(0, (a, b) -> a + b);
+        List<List<Integer>> validTickets = parseTickets(nearbyTicket)
+                .stream()
+                .filter(e -> isValueValue(e, allranges))
+                .collect(Collectors.toList());
+        List<Integer> resultIndexes = new ArrayList<>();
+        List<Pair<Integer, List<Integer>>> possibleIndexes = new ArrayList<>();
+        int position = 0;
+        for (String key : fields) {
+            List<Integer> indexes = findIndexWhereAllValuesMatch(ranges.get(key), validTickets);
+            Pair<Integer, List<Integer>> positionWithPossibleIndexes = new Pair<>(position, indexes);
+            possibleIndexes.add(positionWithPossibleIndexes);
+            position++;
+        }
+        possibleIndexes.sort(Comparator.comparing(integerListPair -> integerListPair.getValue().size()));
+        int[] result = new int[split.length];
+        for (Pair<Integer, List<Integer>> indexInner : possibleIndexes) {
+            for (Integer integer : indexInner.getValue()) {
+                if (!resultIndexes.contains(integer)) {
+                    resultIndexes.add(integer);
+                    result[indexInner.getKey()] = integer;
+                    break;
+                }
+            }
+        }
+        long sum = Long.parseLong(split[result[0]]);
+        for (int j = 1; j < 6; j++) {
+            sum = sum * Long.parseLong(split[result[j]]);
         }
         return sum;
     }
 
-    private static List<Integer> isValueValue(List<Integer> integers, List<Pair<Integer, Integer>> ranges) {
+    private static List<String> getFields(List<String> lines) {
+
+        List<String> files = new ArrayList<>();
+        for (String line : lines) {
+            if (line.isEmpty()) {
+                break;
+            }
+            String[] split = line.split(": ");
+            String fieldName = split[0];
+            files.add(fieldName);
+        }
+        return files;
+    }
+
+    private static List<Integer> findIndexWhereAllValuesMatch(List<Pair<Integer, Integer>> ranges, List<List<Integer>> validTickets) {
+        List<Integer> matchingIndexes = new ArrayList<>();
+        for (int column = 0; column < validTickets.get(0).size(); column++) {
+            int matches = 0;
+            for (List<Integer> ticket : validTickets) {
+                if (isValidRange(ticket.get(column), ranges)) {
+                    matches++;
+                }
+            }
+            if (matches == validTickets.size()) {
+                matchingIndexes.add(column);
+            }
+        }
+        return matchingIndexes;
+    }
+
+    private static boolean isValidRange(Integer integer, List<Pair<Integer, Integer>> ranges) {
+        boolean match = false;
+        for (Pair<Integer, Integer> rule : ranges) {
+            if (matchesRange(rule, integer)) {
+                match = true;
+            }
+        }
+        return match;
+    }
+
+    private static boolean isValueValue(List<Integer> integers, List<Pair<Integer, Integer>> ranges) {
         List<Integer> invalidValues = new ArrayList<>();
         for (Integer number : integers) {
             boolean match = false;
@@ -39,7 +105,7 @@ public class TicketTranslation {
                 invalidValues.add(number);
             }
         }
-        return invalidValues;
+        return invalidValues.isEmpty();
     }
 
     private static List<Pair<Integer, Integer>> getAllRanges(HashMap<String, List<Pair<Integer, Integer>>> ranges) {
@@ -60,7 +126,7 @@ public class TicketTranslation {
         for (String ticket : nearbyTicket) {
             String[] parsedTicket = ticket.split(",");
             List<Integer> parsedTicketNumbers = new ArrayList<>();
-            for(String s : parsedTicket) {
+            for (String s : parsedTicket) {
                 parsedTicketNumbers.add(Integer.parseInt(s));
             }
             tickets.add(parsedTicketNumbers);
@@ -72,7 +138,7 @@ public class TicketTranslation {
         int emptyLineCount = 0;
         List<String> nearbyTickets = new ArrayList<>();
         for (String line : lines) {
-            if (emptyLineCount > 1 && !line.contains("nearby tickets:")){
+            if (emptyLineCount > 1 && !line.contains("nearby tickets:")) {
                 nearbyTickets.add(line);
             }
             if (line.isEmpty()) {
--- src/test/java/day16/TicketTranslationTest.java
+++ src/test/java/day16/TicketTranslationTest.java
@@ -15,7 +15,17 @@ public class TicketTranslationTest {
 
     @Test
     public void part1_case2() throws FileNotFoundException {
-        Assert.assertEquals(71, TicketTranslation.getMemorySum("day16.csv"));
+        Assert.assertEquals(21081, TicketTranslation.getMemorySum("day16.csv"));
+    }
+
+    @Test
+    public void part2_case1() throws FileNotFoundException {
+        Assert.assertEquals(71, TicketTranslationPart2.getMemorySum("day16_test_part2.csv"));
+    }
+
+    @Test
+    public void part2_case2() throws FileNotFoundException {
+        Assert.assertEquals(314360510573L, TicketTranslationPart2.getMemorySum("day16.csv"));
     }
 
 }
